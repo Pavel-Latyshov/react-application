4b60ec7b0ba9e7eb1ab90e7f0a7009c006087a53
Pavel Latyshov <platyshov@MacBook-Pro-Pavel.local>
2021-03-19 20:48:26 +0300

    feat: create first page

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.flaticon.com/svg/vstatic/svg/4195/4195791.svg?token=exp=1616174232~hmac=27035e250aaae236bfc1d6df955b3c40"></img>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+
+const Profile = () => {
+  return (
+    <div className="content">
+    <div>
+      <img src="https://images.pexels.com/photos/2690323/pexels-photo-2690323.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"></img>
+    </div>
+    <div className="profile-data">
+    <div>Ava + description</div>
+    </div>
+    <div className="post">
+    <div>My posts</div>
+    <div>New Post</div>
+    <div>Post1</div>
+    <div>Post2</div>
+    </div>
+  </div>
+  )
+}
+
+export default Profile
